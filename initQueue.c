c9065622515e16d5a8a61de792c9e122226fe7b5
akochenkov <alexandr.kochenkov@gmail.com>
2021-02-10 00:48:09 -0800

    Finalized. Finished.

--- initQueue.c
+++ initQueue.c
@@ -1,7 +1,16 @@
+/////////////////////////////////////////////////////////////////////////////
+//   Name: Alexandr Kochenkov        Date: 2/10/2021                       //
+//   Class: CSE222A                  Assignment: Pogramming Assignment 3   //
+//                                                                         //
+//   Functions purpose: This functions purpose is to init the linker list  //
+//   queue. It first makes space for the list and sent the next to NULL.   //
+//                                         				   //
+//                                                                         //
+/////////////////////////////////////////////////////////////////////////////
 #include "main.h"
 
-struct node *initQueue() {    //makes the list clean and sets the sentinel node
-  struct node *s=malloc(sizeof(struct node));  //this allocates a block og memory for s
+struct node *initQueue() {    //makes the Queue list clean and sets the sentinel node
+  struct node *s=malloc(sizeof(struct node));  //this allocates a block of memory for s
   s->next=NULL;  //makes the sentinel node s next be NULL
   return(s);     //returns the s, which is the cleaned list or the sentinel node
 
