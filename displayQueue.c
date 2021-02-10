8ac83e3d70c744d450928db2bd909b2e166cb5df
akochenkov <alexandr.kochenkov@gmail.com>
2021-02-10 00:24:44 -0800

    Finalized. Added stuff.

--- displayQueue.c
+++ displayQueue.c
@@ -1,11 +1,25 @@
+/////////////////////////////////////////////////////////////////////////////
+//   Name: Alexandr Kochenkov        Date: 2/10/2021                       //
+//   Class: CSE222A                  Assignment: Pogramming Assignment 3   //
+//                                                                         //
+//   Functions purpose:  This function prints out the linker list of the   //
+//   queue list. It moves down the stored list and prints out the data.    //
+//   									   //
+//                                                                         //
+/////////////////////////////////////////////////////////////////////////////
 #include "main.h"
 
 void displayQueue(struct node *sent) {
-  sent=sent->next;
-  while(sent != NULL) {
-    printf("%d ", sent->data);
-    sent=sent->next;
+  if(sent->next == NULL) {                    //checks if there are any nodes to print or it will seg fault
+    printf("Queue is empty\n");               //prints message and return to the main program
+    return;
   }
-  printf("\n");
+  sent=sent->next;                //next node of the sentinel node
+  printf("HEAD--> ");            
+  while(sent != NULL) {           //goes down the list while next is not NULL
+    printf("%d ", sent->data);    //prints the data of the nodes
+    sent=sent->next;              //goes to the next node
+  }
+  printf("<-- TAIL\n");           //prints the tail
 
 }
